9b0725a2311f51989d4446b1446ce6cc4b06923d
fahad3853 <129646080+fahad3853@users.noreply.github.com>
2026-02-08 21:40:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+messages = [
+    "You are the most beautiful part of my life ❤️",
+    "Every moment with you feels magical ✨",
+    "I fall in love with you again every day 💕",
+    "You are my peace, my happiness, my home 🏡❤️"
+]
+
+@app.route("/")
+def home():
+    message = random.choice(messages)
+    return render_template("index.html", message=message)
+
+if __name__ == "__main__":
+    app.run()
+
