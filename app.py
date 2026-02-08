cc3c77c90565bc5323bfb9cd7500a8e1f753eeca
fahad3853 <129646080+fahad3853@users.noreply.github.com>
2026-02-08 22:39:58 +0530

    Refactor home function to simplify return values

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ def home():
 
     return render_template(
         "index.html",
-        message=message,
         greeting=greeting,
         name=wife_name
     )
@@ -37,3 +36,4 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
